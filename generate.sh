9a6cc22971fc0c7ee549218ef27cd78cbd6e0df7
Florian Esser <android-dev@fesr.de>
2019-02-25 02:26:51 +0100

    Set product model to moto g⁶

--- generate.sh
+++ generate.sh
@@ -74,7 +74,7 @@ $rom_script
 PRODUCT_NAME := $target
 PRODUCT_DEVICE := phhgsi_${arch}_$part
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := Phh-Treble $apps_name
+PRODUCT_MODEL := moto g⁶
 
 PRODUCT_PACKAGES += $extra_packages
 EOF
